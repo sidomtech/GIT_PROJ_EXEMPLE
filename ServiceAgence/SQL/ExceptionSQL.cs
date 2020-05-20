79c1776a01132b753af73937bb22743de67fb48c
domimique ROCHER <dom.rocher73@gmail.com>
2020-05-20 17:46:07 +0200

    Modif SQL 006 - ExceptionSQL.cs

--- ServiceAgence/SQL/ExceptionSQL.cs
+++ ServiceAgence/SQL/ExceptionSQL.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// ajout commeentaire 006
 namespace ServiceAgence.SQL
 {
     class ExceptionSQL : Exception
     {
         private string _requeteSql;
-
         public ExceptionSQL(string message)
             : base(message)
         {
         }
-
+		//
         public ExceptionSQL(string message, string requeteSQL, Exception inner)
             : base(String.Format("{0}\r\nSQL : {1}", message, requeteSQL), inner)
         {
